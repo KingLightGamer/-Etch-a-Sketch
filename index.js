d59e06a72a542498afc33827158942c0cf230e34
KingLightGamer <208028170+KingLightGamer@users.noreply.github.com>
2025-07-13 23:31:32 +0200

    i set a border to make the cells visible but in the last line there are olny 4 columns

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 const container = document.getElementById('container');
 const gridSize = 16; 
 const totalSquares = gridSize * gridSize;
+
+for (let i = 0; i < totalSquares; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+  container.appendChild(square);
+}
