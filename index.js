6cda3f4424c3bb45939eacd67625872cb0acd112
KingLightGamer <208028170+KingLightGamer@users.noreply.github.com>
2025-07-14 00:10:16 +0200

    Now the squares change the color to lightblue if you hover over them. In Js I used addEventListener and the events were  mouseenter and mouseleave

--- index.js
+++ index.js
@@ -6,5 +6,15 @@ const totalSquares = gridSize * gridSize;
 for (let i = 0; i < totalSquares; i++) {
   const square = document.createElement('div');
   square.classList.add('square');
+  square.addEventListener('mouseenter', () => {
+    square.style.backgroundColor = 'lightblue';
+  });
+
+  square.addEventListener('mouseleave', () => {
+    square.style.backgroundColor = 'white';
+  });
+
+  
   container.appendChild(square);
 }
+
